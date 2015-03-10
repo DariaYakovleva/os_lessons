f9dc42de07c00bb4378fe2eca05389cf5ba50dec
Daria Yakovleva <dariyakovleva@gmail.com>
2015-03-10 23:52:24 +0400

    add error handling

--- cat/cat.c
+++ cat/cat.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main() {
     size_t const MAX_LEN = 10000;
     void *buf = malloc(MAX_LEN);
-    ssize_t len = read_(STDIN_FILENO, buf, MAX_LEN);
-    write_(STDOUT_FILENO, buf, len);
+    ssize_t len;
+    while ((len = read_(STDIN_FILENO, buf, MAX_LEN)) > 0) {
+        write_(STDOUT_FILENO, buf, len);
+    }
+    if (len < 0) {
+        write_(STDERR_FILENO, "read error", 5);
+    }
     free(buf);
     return 0;
 }
--- lib/helpers.h
+++ lib/helpers.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <errno.h>
 
 ssize_t read_ (int fd, void * buffer, size_t count);
 ssize_t write_ (int fd, const void * buffer, size_t count);
